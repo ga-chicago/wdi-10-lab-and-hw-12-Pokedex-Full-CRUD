5ea2379fcdbcba42276816b9168c6cc892c65021
Travis1282 <tmail20@gmail.com>
2018-01-10 17:03:40 -0600

    got the edit and delete working

--- controllers/pokeController.js
+++ controllers/pokeController.js
@@ -4,9 +4,6 @@ const Pokemon = require('../models/pokemon.js');
 
 
 
-
-
-
 //////////////// INDEX ////////////////
 router.get('/', (req, res) => {
 	res.render('index.ejs', { data: Pokemon });
@@ -14,9 +11,18 @@ router.get('/', (req, res) => {
 });
 
 
+/////////////// DELETE ////////////////
+router.delete('/:id', (req, res) => {
+	console.log('hitting')
+  const idx = req.params.id;
+  Pokemon.splice(idx, 1)
+  res.redirect('/pokemon')
+})
+
 
 ////////////// SHOW ////////////////
 router.get('/:id', (req, res) => {
+	console.log('get route')
     res.render('show.ejs', { 
     	data: Pokemon[req.params.id] 
     });
@@ -24,9 +30,37 @@ router.get('/:id', (req, res) => {
 });
 
 
+/////////////// EDIT ////////////////
+router.get('/:id/edit', (req, res) => {
+  res.render('edit.ejs', {
+    data: Pokemon[req.params.id], 
+    index: req.params.id 
+
+  })
+  // console.log("hey the edit route was hit;")
+
+})
+
+router.put('/:id', (req, res) => {
+
+  console.log("hey update route was hit;")
+
+  Pokemon[req.params.id] = {
+    name: req.body.name,
+  }
+
+  res.redirect('/pokemon');
+})
 
 
-
-
+/////////////// EXPORT ////////////////
 
 module.exports = router;
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -5,52 +5,19 @@ const express    = require('express'),
 	  methodOverride = require('method-override'); /// for delete and possibly put (edit)
 
 
-// FOR DELETE
+// always you to read req.body
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+/////////////// FOR DELETE ///////////////
 app.use(methodOverride('_method'))
-// FOR CSS
+/////////////// FOR CSS ///////////////
 app.use(express.static('public'));
 
 const pokeControler = require('./controllers/pokeController.js')// get the controller functions and require them
 app.use('/pokemon/', pokeControler);//all paths will start with /fruits/
 
-
-// //////////////// INDEX ////////////////
-// app.get('/pokemon/', (req, res) => {
-// 	res.render('index.ejs', { data: Pokemon });
-// 	// res.send("GET ROUTE HIT")
-// });
-
-
-//////////////// SHOW ////////////////
-// app.get('/pokemon/:id', (req, res) => {
-//     res.render('show.ejs', { 
-//     	data: Pokemon[req.params.id] 
-//     });
-//     // res.send("you tried to go to /" + req.params.id)
-// });
-
-
-// /////////////// EDIT ////////////////
-// router.get('/pokemon/:id/edit', (req, res) => {
-//   res.render('edit.ejs', {
-//     data: Pokemon[req.params.id], 
-//     index: req.params.id 
-
-//   })
-//   console.log("hey the edit route was hit;")
-
-// })
-
-// router.put('pokemon/:index', (req, res) => {
-
-//   console.log("hey update route was hit;")
-
-//   Pokemon[req.params.index] = {
-//     name: req.body.name,
-//   }
-
-//   res.redirect('/');
-// })
 
 
 
