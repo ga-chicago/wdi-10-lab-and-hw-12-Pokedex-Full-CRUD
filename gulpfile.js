dd220a94c637f608e9cfac02afca584baeffda48
Anthony <anthonyjlower@gmail.com>
2018-01-10 15:14:07 -0600

    created gulpfile and added css styling to index page

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp");
+const less = require("gulp-less");
+
+gulp.task("less", () => {
+	gulp.src("./less/style.less")
+		.pipe(less())
+		.pipe(gulp.dest("./public/css/"))
+});
+
+gulp.task("watch", () => {
+	gulp.watch("./less/*.less", ["less"])
+})
