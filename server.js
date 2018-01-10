2fffc5d24ba2be911c88747c1f9c8ba9dcf41ef3
Michael Weber <webermn15@gmail.com>
2018-01-10 16:32:24 -0600

    set up new.ejs routes, redirects back to main page after as well. cool

--- server.js
+++ server.js
@@ -21,16 +21,55 @@ app.get('/', (req, res) => {
 	// res.send(Pokemon);
 });
 
-// SHOW
-app.get('/:id', (req, res) => {
-	console.log('hit it');
-    res.render('show.ejs', { data: Pokemon[req.params.id] });
-});
-
 // NEW
 app.get('/new', (req, res) => {
 	res.render('new.ejs');
-})
+});
+
+// SHOW
+app.get('/:id', (req, res) => {
+    res.render('show.ejs', { data: Pokemon[req.params.id] });
+});
+
+app.post('/', (req, res) => {
+	console.log(req.body);
+
+	let typeArr = [];
+
+	if (req.body.typeOne) {
+		typeArr.push(req.body.typeOne)
+	}
+	if (req.body.typeTwo) {
+		typeArr.push(req.body.typeTwo)
+	}
+
+	let miscObj = {
+		weight: req.body.weight,
+		height: req.body.height
+	}
+
+	let statsObj = {
+		hp: req.body.hp,
+		attack: req.body.attack,
+		defense: req.body.defense,
+		spattack: req.body.spattack,
+		spdefense: req.body.spdefense,
+		speed: req.body.speed
+	}
+
+	Pokemon.push({
+		img: req.body.img,
+		id: req.body.id,
+		name: req.body.name,
+		type: typeArr,
+		misc: miscObj,
+		stats: statsObj
+	});
+
+	res.redirect('/');
+	// res.send('it posted bitch')
+});
+
 
 
 
