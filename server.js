05cc01434f793f5eb30bb498c7619689d057f70b
Anthony <anthonyjlower@gmail.com>
2018-01-10 14:26:07 -0600

    Listening on Port 3000 and set up test route

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+	res.send("Hit main index")
+})
+
+
+
+
+
+
+app.listen(port, () => {
+	console.log("Listening on port 3000")
+})
