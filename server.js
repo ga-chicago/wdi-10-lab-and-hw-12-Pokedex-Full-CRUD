1a528e97d903ed318c6fa7d5c78a8801d4dd3993
Michael Weber <webermn15@gmail.com>
2018-01-11 10:37:12 -0600

    basic styling, works great

--- server.js
+++ server.js
@@ -24,6 +24,45 @@ app.get('/', (req, res) => {
 });
 
 
+//EDIT
+app.put('/:id', (req, res) => {
+
+	let typeArr = [];
+
+	if (req.body.typeOne) {
+		typeArr.push(req.body.typeOne)
+	}
+	if (req.body.typeTwo) {
+		typeArr.push(req.body.typeTwo)
+	}
+
+	let miscObj = {
+		weight: req.body.weight,
+		height: req.body.height
+	}
+
+	let statsObj = {
+		hp: req.body.hp,
+		attack: req.body.attack,
+		defense: req.body.defense,
+		spattack: req.body.spattack,
+		spdefense: req.body.spdefense,
+		speed: req.body.speed
+	}
+
+	Pokemon[req.params.id] = {
+		img: req.body.img,
+		id: req.body.id,
+		name: req.body.name,
+		type: typeArr,
+		misc: miscObj,
+		stats: statsObj
+	};
+
+	res.redirect('/');
+});
+
+
 // NEW
 app.get('/new', (req, res) => {
 	res.render('new.ejs');
@@ -36,7 +75,8 @@ app.get('/:id/edit', (req, res) => {
 	// YOU STILL NEED TO LOOP THE TYPES IN THE EDIT.EJS FILE DON'T FORGET
 
 	res.render('edit.ejs', {
-		data: Pokemon[req.params.id]
+		data: Pokemon[req.params.id],
+		index: req.params.id
 	});
 });
 
