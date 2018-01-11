1a97341d98d960eeb0b5325041000fd856303fbb
sergio <scrummish@gmail.com>
2018-01-11 00:35:53 -0600

    Added basic pokedex view

--- controllers/pokedexController.js
+++ controllers/pokedexController.js
@@ -2,9 +2,32 @@ const express = require('express');
 const router = express.Router();
 const pokeBase = require('../models/pokemon.js');
 
-router.route('/')
+router.route('/') //  Create the pokedex landing page
 	.get((request,response)=>{
-		res.render('index.ejs',{pokemon: pokeBase});
+		response.render('index.ejs',{pokemon: pokeBase});
+	})
+
+router.route('/new') // Create a new pokemon in the pokedex
+	.get((request,response)=>{
+		response.render('new.ejs');
+		response.redirect('/pokemon/');
+	})
+
+router.route('/:index') // Show the chosen pokemon 
+	.get((request,response)=>{
+		response.render('show.ejs',{pokemon: pokeBase[request.params.index]});
+	})
+	.delete((request,response)=>{
+		pokeBase.splice(request.params.index,1);
+		response.redirect('/pokemon/');
+	})
+
+router.route('/:index/edit') // Shows user a form to edit the chosen pokemon
+	.get((request,response)=>{
+		response.render('edit.ejs',{
+			index: request.params.index,
+			pokemon: pokeBase[request.params.index]
+		});
 	})
 
 module.exports = router;
